c9a913ab89f7a6bf83e6bcb7188a915b18e6d334
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-02-24 18:29:54 +0200

    Add project files.

---
+++ YearsApplication/YearsApplication/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YearsApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int x = 1990;
+            for (int i = 0; i < 40; i++)
+            {
+                MyLabel.Text += x.ToString() + " , ";
+                x ++;
+            }
+            MyLabel.Text += x.ToString();
+        }
+    }
+}
